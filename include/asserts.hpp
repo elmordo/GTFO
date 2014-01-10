a0ebd2288f6983bb08744f7567905b082497b082
Bc. Petr Jindra <el.mordo@gmail.com>
2014-01-10 20:07:28 +0100

    Pridany dalsi funkce assertaci

--- include/asserts.hpp
+++ include/asserts.hpp
@@ -83,6 +83,72 @@ void assertLessOrEqual(G g, E e, string msg) {
 	}
 }
 
+/**
+ * asertace, jestli ukazatel ukazuje na spravnou instanci
+ */
+template<class E>
+void assertInstanceOf(void *g, string msg) {
+	if (dynamic_cast<E*>(g) == 0) {
+		throw AssertException(msg);
+	}
+}
+
+/**
+ * asertace, jestli g a e jsou stejne instance (stejne misto v pameti)
+ */
+template<class T>
+void assertIdentical(T &g, T &e, string msg) {
+	if (&g != &e) {
+		throw AssertException(msg);
+	}
+}
+
+/**
+ * asertace, jestli g a e jsou ruzne instance (ruzne misto v pameti)
+ */
+template<class T>
+void assertNotIdentical(T &g, T &e, string msg) {
+	if (&g == &e) {
+		throw AssertException(msg);
+	}
+}
+
+/**
+ * asertace, jestli je dany ukazatel NULL (0x0)
+ */
+void assertNull(void *g, string msg) {
+	if (g != 0x0) {
+		throw AssertException(msg);
+	}
+}
+
+/**
+ * asertace, jestli dany ukazatel neni NULL (0x0)
+ */
+void assertNotNull(void *g, string msg) {
+	if (g == 0x0) {
+		throw AssertException(msg);
+	}
+}
+
+/**
+ * asertace, jestli je dana hodnota True
+ */
+void assertTrue(bool g, string msg) {
+	if (!g) {
+		throw AssertException(msg);
+	}
+}
+
+/**
+ * asertace, jestli je dana hodnota False
+ */
+void assertFalse(bool g, string msg) {
+	if (g) {
+		throw AssertException(msg);
+	}
+}
+
 }
 }
 
